553c10365142e043fda320e7f3761aba32e631fe
klinkl <108584514+klinkl@users.noreply.github.com>
2023-03-17 14:15:39 +0000

    updated Spaceship and added bullet

--- app/src/main/java/com/example/projectapplication/SpaceGameView.java
+++ app/src/main/java/com/example/projectapplication/SpaceGameView.java
@@ -1,51 +1,78 @@
 package com.example.projectapplication;
 
+
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import java.io.IOException;
 
 public class SpaceGameView extends SurfaceView implements Runnable{
 
     private Context context;
 
+    // This is our thread
     private Thread gameThread = null;
+
+    // Our SurfaceHolder to lock the surface before we draw our graphics
     private SurfaceHolder ourHolder;
 
+    // A boolean which we will set and unset
+    // when the game is running- or not.
     private volatile boolean playing;
 
+    // Game is paused at the start
     private boolean paused = true;
 
+    // A Canvas and a Paint object
     private Canvas canvas;
     private Paint paint;
 
+    // This variable tracks the game frame rate
     private long fps;
 
+    // This is used to help calculate the fps
     private long timeThisFrame;
+
+    // The size of the screen in pixels
     private int screenX;
     private int screenY;
 
-    private int score = 0;
+    // The score
+    public int score = 0;
 
-    private int lives = 3;
+    // Lives
+    private int lives = 4;
 
 
+    private Spaceship spaceShip;
+    private Bullet bullet;
+    private Bitmap bitmapback;
 
+
+    // This special constructor method runs
     public SpaceGameView(Context context, int x, int y) {
 
+        // The next line of code asks the
+        // SurfaceView class to set up our object.
+        // How kind.
         super(context);
 
-
+        // Make a globally available copy of the context so we can use it in another method
         this.context = context;
 
-
+        // Initialize ourHolder and paint objects
         ourHolder = getHolder();
         paint = new Paint();
 
@@ -61,30 +88,32 @@ public class SpaceGameView extends SurfaceView implements Runnable{
 
     private void initLevel(){
 
-
-
+        spaceShip = new Spaceship(context, screenX, screenY);
+        bullet = new Bullet(screenY,screenX);
     }
 
 
     @Override
     public void run() {
         while (playing) {
+            score = 10;
+            // Capture the current time in milliseconds in startFrameTime
+            long startFrameTime = System.currentTimeMillis();
 
-
-            long startTime = System.currentTimeMillis();
-
-
+            // Update the frame
             if(!paused){
                 update();
             }
 
-
+            // Draw the frame
             draw();
 
-
-            long timeThis = System.currentTimeMillis() - startTime;
-            if (timeThis >= 1) {
-                fps = 1000 / timeThis;
+            // Calculate the fps this frame
+            // We can then use the result to
+            // time animations and more.
+            timeThisFrame = System.currentTimeMillis() - startFrameTime;
+            if (timeThisFrame >= 1) {
+                fps = 1000 / timeThisFrame;
             }
 
         }
@@ -95,35 +124,82 @@ public class SpaceGameView extends SurfaceView implements Runnable{
 
     private void update(){
 
+        spaceShip.update(fps);
 
+        if(bullet.getStatus())
+            bullet.update(fps);
+
+        checkCollisions();
+
+    }
+
+
+    private void checkCollisions(){
+        //  if (spaceShip.getX() > screenX - spaceShip.getLength())
+        //     spaceShip.setX(0);
+        //  if (spaceShip.getX() < 0 + spaceShip.getLength())
+        //      spaceShip.setX(screenX);
+
+        //   if (spaceShip.getY() > screenY - spaceShip.getLength())
+        //       spaceShip.setY(0);
+        //   if (spaceShip.getY() < 0 + spaceShip.getLength())
+        //       spaceShip.setY(screenY);
+
+        if(bullet.getImpactPointY() < 0)
+            bullet.setInactive();
+        if(bullet.getImpactPointY() > screenY)
+            bullet.setInactive();
+
+        if(bullet.getImpactPointX() < 0)
+            bullet.setInactive();
+        if(bullet.getImpactPointX() > screenX)
+            bullet.setInactive();
 
     }
 
 
 
 
-
-
     private void draw(){
-
+        // Make sure our drawing surface is valid or we crash
         if (ourHolder.getSurface().isValid()) {
-
+            // Lock the canvas ready to draw
             canvas = ourHolder.lockCanvas();
 
-
+            // Draw the background color
             canvas.drawColor(Color.argb(255, 26, 128, 182));
 
+            // Choose the brush color for drawing
+            paint.setColor(Color.argb(255,  255, 255, 255));
+
+            bitmapback = BitmapFactory.decodeResource(context.getResources(), R.drawable.background);
+            bitmapback = Bitmap.createScaledBitmap(bitmapback, (int) (screenX), (int) (screenY),false);
+            //  canvas.drawBitmap(background.getBitmap(), spaceShip.getX(), spaceShip.getY() , paint);
+            //  draw the defender
+            canvas.drawBitmap(bitmapback, 0, 0, paint);
+            canvas.drawBitmap(spaceShip.getBitmap(), spaceShip.getX(), spaceShip.getY() , paint);
+
+
+            if(bullet.getStatus())
+                canvas.drawRect(bullet.getRect(), paint);
+
+
+            // Draw the score and remaining lives
+            // Change the brush color
             paint.setColor(Color.argb(255,  249, 129, 0));
             paint.setTextSize(40);
-            canvas.drawText("Score: " + score + "   Lives: " +
-                    lives, 10,50, paint);
+            canvas.drawText("Score: " + score + "   Lives: " + lives + "    FPS: " + fps, 10,50, paint);
 
+            // Draw everything to the screen
             ourHolder.unlockCanvasAndPost(canvas);
         }
     }
 
 
 
+
+    // If SpaceGameActivity is paused/stopped
+    // shutdown our thread.
     public void pause() {
         playing = false;
         try {
@@ -134,11 +210,49 @@ public class SpaceGameView extends SurfaceView implements Runnable{
 
     }
 
+
+
+    // If SpaceInvadersActivity is started then
+    // start our thread.
     public void resume() {
         playing = true;
         gameThread = new Thread(this);
         gameThread.start();
     }
+
+
+    // The SurfaceView class implements onTouchListener
+    // So we can override this method and detect screen touches.
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+
+            // Player has touched the screen
+
+            case MotionEvent.ACTION_MOVE:
+
+
+                paused = false;
+                spaceShip.setX((int)motionEvent.getX());
+                spaceShip.setY((int)motionEvent.getY());
+                bullet.shoot(spaceShip.getX(),spaceShip.getY()+ spaceShip.getHeight()/2,0);
+
+
+                break;
+
+            // Player has removed finger from screen
+            case MotionEvent.ACTION_UP:
+
+                //   if(motionEvent.getY() > screenY - screenY / 10) {
+                spaceShip.setMovementState(spaceShip.STOPPED);
+                //   }
+                break;
+        }
+        return true;
+    }
+
+
 
 
 
